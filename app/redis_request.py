18f833e0a019ba863340e2284b6132fdeacb4f04
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-03-23 22:42:02 -0400

    Testing Changes

--- app/redis_request.py
+++ app/redis_request.py
@@ -2,14 +2,29 @@ from app.redis_encoder import RedisEncoder
 from app.redis_parser import RedisParser
 
 class RedisRequest:
-    def __init__(self, request) -> None:
-        self.data = RedisParser.parse_value(request)
-        self.command = self.data[0].lower()
+    # def __init__(self, request) -> None:
+    #     self.data = RedisParser.parse_value(request)
+    #     self.command = self.data[0].lower()
         
-    def response(self)-> bytes:
+    # def response(self)-> bytes:
+    #     if self.command == "ping":
+    #         return RedisEncoder.encode_simple_string("PONG")
+    #     elif self.command == "echo":
+    #         return RedisEncoder.encode_bulk_string(self.data[1])
+    
+    def __init__(self, request_bytes: bytes):
+        self.data = RedisParser().parse(request_bytes)
+        self.command = self.data[0].lower()
+    def response(self) -> bytes:
+        """
+        Returns the response for the current request
+        :return: The response for the current request in bytes
+        """
         if self.command == "ping":
             return RedisEncoder.encode_simple_string("PONG")
         elif self.command == "echo":
             return RedisEncoder.encode_bulk_string(self.data[1])
+    def __str__(self):
+        return str(self.data)
     
     
