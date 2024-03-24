e8e72e2923ce4f2679c3a1bad8a325cde0753092
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-03-23 20:04:56 -0400

    Fourth Stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 import socket
 import threading
 
+def respond(conn):
+    response = "+PONG\r\n"
+    with conn:
+        while req := conn.recv(1024):
+            conn.send(response.encode())
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,18 +13,11 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 6379))
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while connection := server_socket.accept():
         conn, addr = connection
-        thread = threading.Thread(target=respond, args=[conn])
+        thread = threading.Thread(target=respond, args=[connection])
         thread.start()
-    
-    def respond(conn):
-        res = "+PONG\r\n"
-        with conn:
-            while req := conn.recv(1024):
-                conn.sendall(res.encode())
-            
-
+        
 if __name__ == "__main__":
     main()
