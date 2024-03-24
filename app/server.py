06adfdb719084b10ae73684043bceac2c9261ea3
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-03-24 15:14:38 -0400

    Change client_thread method in server.py

--- app/server.py
+++ app/server.py
@@ -12,23 +12,10 @@ class Server:
         server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
         while True:
             connection, address = server_socket.accept()
-            thread = threading.Thread(
-                target=self.client_thread, args=(connection, address)
-            )
+            thread = threading.Thread(target=self.new_client, args=(connection, address))
             thread.start()
 
-    def client_thread(self, connection, address):
-        # conn, addr = connection
-        # while True:
-        #     request = conn.recv(1024)
-        #     if not request:
-        #         connection.close()
-        #         break
-        #     redis_request = RedisRequest(request)
-        #     redis_response = redis_request.response(self)
-        #     connection.sendall(redis_response)
-        # connection.close()
-        
+    def new_client(self, connection, address):
         while request := connection.recv(1024):
             redis_request = RedisRequest(request)
             redis_response = redis_request.response(self)
