ccb55f5fcf434be988639468e4c93f611d70ffbe
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-03-25 13:12:34 -0400

    Removed hardcoded master_replid

--- app/server.py
+++ app/server.py
@@ -2,21 +2,27 @@ import socket
 import threading    
 from app.redis_request import RedisRequest
 from app.database import Database
-#haflhfasdljfhjkaksjfdhasjk
+from random import random
+import string
+
 class Server:
     def __init__(self, port=6379, replica=[]):
         self.database = Database()
         
+        # Default Server Settings
+        self.master = True
         self.role = "master"
+        self.replica = []
+        
+        self.master_replid= (''.join(random.choices(string.ascii_lowercase, k=40))) # Needs to changed to a random 40 char alphanum string
+        self.master_repl_offset="0"
+        
         if replica:
             self.role = "slave"
+            self.replica = replica
         
-        self.master_replid="8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
-        self.master_repl_offset="0"
+       
          
-            
-        
-        
         self.port = port
     def run(self):
         server_socket = socket.create_server(("localhost", self.port), reuse_port=True)
@@ -30,7 +36,9 @@ class Server:
         while request := connection.recv(1024):
             redis_request = RedisRequest(request)
             redis_response = redis_request.response(self)
+            
             connection.sendall(redis_response)
             
-    
+    def isReplica(self):
+        pass
     
